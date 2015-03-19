e6c34393035ae7ed73ba64d325366af85626b795
ToshiZ <andi4andi@gmail.com>
2015-03-19 06:19:31 +0300

    v1.0.3. add users admins

--- pyr_int/db/schema.rb
+++ pyr_int/db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316050018) do
+ActiveRecord::Schema.define(version: 20150319031445) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
 
   create_table "ssh_connections", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150316050018) do
     t.string   "login"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "password"
   end
 
   create_table "task_templates", force: :cascade do |t|
@@ -109,5 +128,31 @@ ActiveRecord::Schema.define(version: 20150316050018) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "users_connections", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "ssh_connection_id"
+  end
+
+  add_index "users_connections", ["ssh_connection_id"], name: "index_users_connections_on_ssh_connection_id"
+  add_index "users_connections", ["user_id"], name: "index_users_connections_on_user_id"
 
 end
